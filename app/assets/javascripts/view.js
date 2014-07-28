d3de0d23d7a0f960899a1587634a06050d7dd330
Kritika <kratikasaxena2808@gmail.com>
2014-07-29 01:32:29 +0530

    Observer pattern without on and trigger implemented

--- app/assets/javascripts/view.js
+++ app/assets/javascripts/view.js
@@ -6,13 +6,16 @@ var Calculator = function(calculatorDiv){
     this.inputCommand = $(calculator).find(".inputCommand");
     this.submitButton = $(calculator).find(".submitButton");
     this.resultHistory = $(calculator).find(".resultHistory");
-
+    this.observerList=[];
     this.initialize();
 }
 
 Calculator.prototype={
     initialize:function(){
         this.observeButtonClick();
+    },
+    registerObserver:function(observer){
+        this.observerList.push(observer)
     },
     observeButtonClick:function(){
         this.submitButton.click(_.bind(this.createCalculator, this));
@@ -48,7 +51,11 @@ Calculator.prototype={
             url: "http://localhost:3000/api/calculator",
             data: {command : self.inputCommand.val()},
             success: function (result) {
-                $(self.resultHistory).append("<h4>The output for input command <i>"+self.inputCommand.val() +"</i> is: </h4>"+result.state+"<br><hr>");
+               $(self.resultHistory).append("<h4>The output for input command <i>"+self.inputCommand.val() +"</i> is: </h4>"+result.state+"<br><hr>");
+                $.each(self.observerList, function(index, value) {
+                    $(value.resultHistory).append("<h4>The output for input command <i>"+self.inputCommand.val() +"</i> is: </h4>"+result.state+"<br><hr>");
+                });
+
                 $("#result:even").css("background-color","darkcyan");
                 $("#result:odd").css("background-color","blue");
             },
@@ -57,10 +64,16 @@ Calculator.prototype={
             }
         });
     }
+    //,
+//    appendToHistory:function(){
+//        $(self.resultHistory).append("<h4>The output for input command <i>"+self.inputCommand.val() +"</i> is: </h4>"+result.state+"<br><hr>");
+//    }
 
 }
 
 $(document).ready(function() {
    var  calculator1 = new Calculator("calculatorDiv");
    var  calculator2 = new Calculator("calculatorDiv2");
+    calculator1.registerObserver(calculator2);
+    calculator2.registerObserver(calculator1);
 });
