06992b6987558d8d1d2ad5896f56d76e5f0fb24a
Kritika <kratikasaxena2808@gmail.com>
2014-07-28 17:40:44 +0530

    Removed all the anonymous function

--- app/assets/javascripts/view.js
+++ app/assets/javascripts/view.js
@@ -15,32 +15,33 @@ Calculator.prototype={
         this.observeButtonClick();
     },
     observeButtonClick:function(){
-        var self = this;
-        $(this.submitButton).click(function(){
-            if(self.calculatorState != "created"){
-                self.create();
-            }
-            self.call_update();
-      })
+        this.submitButton.click(_.bind(this.createCalculator, this));
     },
-    create:function(){
+    createCalculator:function(){
+        if(this.calculatorState != "created"){
+            this.callCreate();
+        }
+        this.callUpdate();
+    },
+    callCreate:function(){
         var self=this;
         $.ajax({
             type: "POST",
             url: "http://localhost:3000/api/calculator"
-        }).success(function(result,statusText,xhr){
-            if (xhr.status == 201)
-            {
-                $(self.resultHistory).append("<h4>Calculator created</h4>");
-                self.calculatorState = "created";
-            }
-            else if(xhr.status == 200){
-                $(self.resultHistory).append("<h4>Calculator found</h4>");
-                self.calculatorState = "created";
-            }
-        })
+        }).success(_.bind(self.handleCallCreate,self))
     },
-    call_update:function(){
+    handleCallCreate:function(result,statusText,xhr){
+        if (xhr.status == 201)
+        {
+            $(self.resultHistory).append("<h4>Calculator created</h4>");
+            self.calculatorState = "created";
+        }
+        else if(xhr.status == 200){
+            $(self.resultHistory).append("<h4>Calculator found</h4>");
+            self.calculatorState = "created";
+        }
+    },
+    callUpdate:function(){
         var self = this;
         $.ajax({
             type: "PUT",
