e9abd9164781a7c0540e84e2920ad8bcf3a171ed
Kritika <kratikasaxena2808@gmail.com>
2014-07-31 22:54:53 +0530

    specs implemented for api controller

--- spec/controllers/calculator_controller_spec.rb
+++ spec/controllers/calculator_controller_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 describe CalculatorController do
   it "response to be 200 on put call" do
+    user = User.create({:email => "k@s.com", :password => "kritikasaxena"})
+    expect(user).to be_valid
+    sign_in user
 	put :update, :command => "add 5"
 	expect(response.status).to eq(200)
 	expect(assigns[:state]).to eq(5.0)
